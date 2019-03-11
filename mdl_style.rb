9d99ff2029f4f0ddc0cb2212a5e717bda937801f
Dennis B <bluz71@users.noreply.github.com>
2019-03-11 16:21:19 +1100

    Tweaked mdlint

--- mdl_style.rb
+++ mdl_style.rb
@@ -8,3 +8,4 @@ exclude_rule 'MD024' # allow multiple headings with the same comment
 exclude_rule 'MD030' # allow spaces after list markers
 exclude_rule 'MD033' # allow inline HTML
 exclude_rule 'MD040' # allow code blocks without language specification
+exclude_rule 'MD041' # allow non-headers on the first line, e.g. meta section
