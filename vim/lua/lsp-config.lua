31dafe1908a6965348e7cddc6f3423f4c19e3c27
Dennis B <bluz71@users.noreply.github.com>
2020-09-24 19:08:03 +1000

    Neovim LSP stuff

--- vim/lua/lsp-config.lua
+++ vim/lua/lsp-config.lua
@@ -5,6 +5,7 @@
 --  https://neovim.io/doc/user/lsp.html
 --  https://github.com/scalameta/nvim-metals
 --  https://github.com/scalameta/nvim-metals/blob/master/nvim-lsp.vim
+--  https://github.com/neovim/neovim/pull/12444
 
 local nvim_lsp   = require'nvim_lsp'
 local completion = require'completion'
@@ -15,12 +16,15 @@ local lsp_on_attach = function(client)
 
   -- Mappings
   local opts = {noremap = true, silent = true}
+  vim.fn.nvim_buf_set_keymap(0, 'n', 'ga','<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
   vim.fn.nvim_buf_set_keymap(0, 'n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
+  vim.fn.nvim_buf_set_keymap(0, 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
   vim.fn.nvim_buf_set_keymap(0, 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
   vim.fn.nvim_buf_set_keymap(0, 'n', 'gR','<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-  vim.fn.nvim_buf_set_keymap(0, 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
-  vim.fn.nvim_buf_set_keymap(0, 'n', 'ga','<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
-  vim.fn.nvim_buf_set_keymap(0, 'n', 'gh', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+  vim.fn.nvim_buf_set_keymap(0, 'i', '<c-h>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+
+  -- Omni function
+  vim.api.nvim_command('setlocal omnifunc=v:lua.vim.lsp.omnifunc')
 end
 
 -- Disable diagnostics, use ALE instead
@@ -33,7 +37,7 @@ vim.g.completion_matching_strategy_list = {'exact'}
 vim.g.completion_menu_length            = 0
 vim.g.completion_sorting                = 'alphabet'
 vim.g.completion_timer_cycle            = 150
-vim.g.completion_trigger_keyword_length = 2
+vim.g.completion_trigger_keyword_length = 3
 
 -- The Language Servers
 nvim_lsp.dartls.setup {
