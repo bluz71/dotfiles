9c1e9ddb01503c2f1c6a21eb9580d5f521447e78
Dennis B <bluz71@users.noreply.github.com>
2020-09-26 12:22:16 +1000

    Neovim LSP tweaks

--- vim/lua/lsp-config.lua
+++ vim/lua/lsp-config.lua
@@ -6,25 +6,26 @@
 local nvim_lsp   = require'nvim_lsp'
 local completion = require'completion'
 
--- Disable diagnostics, use ALE instead
+-- Disable diagnostics, use ALE instead.
 vim.lsp.callbacks["textDocument/publishDiagnostics"] = function() end
 
--- Diagnostics symbols
+-- Diagnostics symbols.
 vim.api.nvim_command('sign define LspDiagnosticsErrorSign text=✖')
 vim.api.nvim_command('sign define LspDiagnosticsWarningSign text=✖')
 vim.api.nvim_command('sign define LspDiagnosticsInformationSign text=●')
 vim.api.nvim_command('sign define LspDiagnosticsHintSign text=●')
 
--- Options for completion-nvim plugin
+-- Options for completion-nvim plugin.
 vim.g.completion_enable_auto_hover      = 0
+vim.g.completion_enable_auto_paren      = 0
 vim.g.completion_enable_auto_signature  = 0
-vim.g.completion_matching_strategy_list = {'exact', 'substring', 'fuzzy'}
+vim.g.completion_matching_strategy_list = {'exact'}
 vim.g.completion_menu_length            = 0
 vim.g.completion_sorting                = 'alphabet'
 vim.g.completion_timer_cycle            = 150
 vim.g.completion_trigger_keyword_length = 3
 
--- Code action client capabilities
+-- Code action client capabilities.
 local client_capabilities = vim.lsp.protocol.make_client_capabilities()
 client_capabilities.textDocument.codeAction = {
   codeActionLiteralSupport = {
@@ -49,11 +50,11 @@ local lsp_on_attach = function(client)
   vim.fn.nvim_buf_set_keymap(0, 'n', 'gR','<cmd>lua vim.lsp.buf.rename()<CR>', opts)
   vim.fn.nvim_buf_set_keymap(0, 'i', '<c-h>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
 
-  -- Enable LSP omnifunc
+  -- Enable LSP omnifunc.
   vim.api.nvim_command('setlocal omnifunc=v:lua.vim.lsp.omnifunc')
 end
 
--- The Language Servers
+-- The Language Servers.
 nvim_lsp.dartls.setup {
   on_attach = lsp_on_attach,
   capabilities = client_capabilities
