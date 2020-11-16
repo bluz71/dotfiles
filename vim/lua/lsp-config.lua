2671ee6292d1c093ada37440ac6c19623a30d1d2
Dennis B <bluz71@users.noreply.github.com>
2020-11-16 15:45:48 +1100

    Neovim LSP API changes

--- vim/lua/lsp-config.lua
+++ vim/lua/lsp-config.lua
@@ -3,17 +3,29 @@
 --  https://rishabhrd.github.io/jekyll/update/2020/09/19/nvim_lsp_config.html
 --  https://neovim.io/doc/user/lsp.html
 
-local nvim_lsp   = require'nvim_lsp'
+local nvim_lsp   = require'lspconfig'
 local completion = require'completion'
 
--- Disable diagnostics, use ALE instead.
-vim.lsp.callbacks["textDocument/publishDiagnostics"] = function() end
+-- Custom diagnostic handler.
+local diagnostic_handler = vim.lsp.with(
+  vim.lsp.diagnostic.on_publish_diagnostics, {
+    signs = true,
+    underline = false,
+    update_in_insert = false,
+    virtual_text = {
+      spacing = 2,
+    },
+  }
+)
 
--- Diagnostics symbols.
-vim.api.nvim_command('sign define LspDiagnosticsErrorSign text=✖')
-vim.api.nvim_command('sign define LspDiagnosticsWarningSign text=✖')
-vim.api.nvim_command('sign define LspDiagnosticsInformationSign text=●')
-vim.api.nvim_command('sign define LspDiagnosticsHintSign text=●')
+-- Empty diagnostic handler.
+local none_diagnostic_handler = function() end
+
+-- Diagnostics symbols for display in the sign column.
+vim.api.nvim_command('sign define LspDiagnosticsSignError text=✖')
+vim.api.nvim_command('sign define LspDiagnosticsSignWarning text=✖')
+vim.api.nvim_command('sign define LspDiagnosticsSignInformation text=●')
+vim.api.nvim_command('sign define LspDiagnosticsSignHint text=●')
 
 -- Options for completion-nvim plugin.
 vim.g.completion_enable_auto_hover      = 0
@@ -29,7 +41,7 @@ vim.g.completion_trigger_keyword_length = 3
 local lsp_on_attach = function(client)
   completion.on_attach(client)
 
-  -- Mappings
+  -- Mappings/
   local opts = {noremap = true, silent = true}
   vim.fn.nvim_buf_set_keymap(0, 'n', 'ga','<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
   vim.fn.nvim_buf_set_keymap(0, 'n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
@@ -37,23 +49,34 @@ local lsp_on_attach = function(client)
   vim.fn.nvim_buf_set_keymap(0, 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
   vim.fn.nvim_buf_set_keymap(0, 'n', 'gR','<cmd>lua vim.lsp.buf.rename()<CR>', opts)
   vim.fn.nvim_buf_set_keymap(0, 'i', '<c-h>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+  vim.fn.nvim_buf_set_keymap(0, 'n', ']w', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
+  vim.fn.nvim_buf_set_keymap(0, 'n', '[w', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
 
   -- Enable LSP omnifunc.
   vim.api.nvim_command('setlocal omnifunc=v:lua.vim.lsp.omnifunc')
 
   -- Indicate that LSP is ready.
-  print("Language server is ready")
+  print('Language server is ready')
 end
 
 -- The Language Servers.
 nvim_lsp.dartls.setup {
   on_attach = lsp_on_attach,
+  handlers = {
+    ['textDocument/publishDiagnostics'] = diagnostic_handler
+  }
 }
 
 nvim_lsp.solargraph.setup {
   on_attach = lsp_on_attach,
+  handlers = {
+    ['textDocument/publishDiagnostics'] = none_diagnostic_handler
+  }
 }
 
 nvim_lsp.tsserver.setup {
   on_attach = lsp_on_attach,
+  handlers = {
+    ['textDocument/publishDiagnostics'] = diagnostic_handler
+  }
 }
