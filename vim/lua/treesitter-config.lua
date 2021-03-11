cc0cb44b94e478811937e6b624c3bba58977eefe
Dennis B <bluz71@users.noreply.github.com>
2021-03-11 16:51:03 +1100

    Neovim treesitter tweaks

--- vim/lua/treesitter-config.lua
+++ vim/lua/treesitter-config.lua
@@ -2,8 +2,8 @@ local treesitter = require'nvim-treesitter.configs'
 
 treesitter.setup {
   ensure_installed = {
-    'c', 'c_sharp', 'cpp', 'dart', 'go', 'html', 'java', 'javascript', 'json',
-    'lua', 'python', 'ruby', 'rust', 'typescript'
+    'c', 'cpp', 'dart', 'go', 'html', 'java', 'javascript', 'python', 'ruby',
+    'rust', 'typescript'
   },
   highlight = {
     enable = true
