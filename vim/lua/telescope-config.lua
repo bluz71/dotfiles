064a2eef065fb1cdd1b2df9ea9bbe6fab6c553c5
Dennis B <bluz71@users.noreply.github.com>
2021-08-11 18:37:18 +1000

    Telescope work

--- vim/lua/telescope-config.lua
+++ vim/lua/telescope-config.lua
@@ -1,10 +1,25 @@
 local telescope = require('telescope')
+local actions = require('telescope.actions')
+
+-- Using delta as previewer:
+--   https://github.com/nvim-telescope/telescope.nvim/issues/605
+--   https://github.com/nvim-telescope/telescope.nvim/issues/609
 
 telescope.setup({
   defaults = {
     layout_config = {
-      prompt_position = 'top'
+      height = 0.8,
+      prompt_position = 'top',
+      preview_width = 0.5,
+      width = 0.9
     },
-    sorting_strategy = 'ascending'
+    mappings = {
+      i = {
+        ["<esc>"] = actions.close,
+      }
+    },
+    prompt_prefix = '❯ ',
+    selection_caret = '▶ ',
+    sorting_strategy = 'ascending',
   }
 })
