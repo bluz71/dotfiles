5d2ac655fddf295e8edcd2b019cf51ef84c25eb6
Dennis B <bluz71@users.noreply.github.com>
2021-08-24 14:58:06 +1000

    hop.nvim mappings

--- vim/lua/hop-config.lua
+++ vim/lua/hop-config.lua
@@ -1,3 +1,6 @@
+local key_map = vim.api.nvim_set_keymap
 local opts = {noremap = true, silent = true}
-vim.api.nvim_set_keymap('n', "''", ':HopWord<CR>', opts)
-vim.api.nvim_set_keymap('n', "'<CR>", ':HopLine<CR>', opts)
+key_map('n', "''", '<cmd>lua require"hop".hint_words()<CR>', opts)
+key_map('x', "''", '<cmd>lua require"hop".hint_words()<CR>', opts)
+key_map('n', "'<CR>", '<cmd>lua require"hop".hint_lines()<CR>', opts)
+key_map('x', "'<CR>", '<cmd>lua require"hop".hint_lines()<CR>', opts)
