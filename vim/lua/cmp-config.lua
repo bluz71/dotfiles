9cb00a1d2b63308f0978f5405eac1225b3ac8261
Dennis B <bluz71@users.noreply.github.com>
2021-08-25 18:08:22 +1000

    nvim-cmp tweaks

--- vim/lua/cmp-config.lua
+++ vim/lua/cmp-config.lua
@@ -4,10 +4,11 @@ cmp.setup({
   completion = {
     keyword_length = 2
   },
+  documentation = false,
   mapping = {
     ['<C-y>'] = cmp.mapping.confirm({
-      behavior = cmp.ConfirmBehavior.Insert,
-      select = true,
+      behavior = cmp.ConfirmBehavior.Replace,
+      select = true
     })
   },
   snippet = {
@@ -21,7 +22,7 @@ vim.cmd [[
   augroup CmpEvents
     autocmd!
     autocmd FileType css,json,scss lua require('cmp').setup.buffer({sources = {{name = 'buffer'}}})
-    autocmd FileType dart,html,javascript,ruby lua require('cmp').setup.buffer({sources = {{name = 'buffer'},{name = "nvim_lsp"},{name = "vsnip"}}})
     autocmd FileType eruby lua require('cmp').setup.buffer({sources = {{name = 'buffer'},{name = "nvim_lsp"}}})
+    autocmd FileType dart,html,javascript,ruby lua require('cmp').setup.buffer({sources = {{name = 'buffer'},{name = "nvim_lsp"},{name = "vsnip"}}})
   augroup END
 ]]
