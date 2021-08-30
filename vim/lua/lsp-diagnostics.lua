6477515834d46f35299f24c3df24647cef8e4ed8
Dennis B <bluz71@users.noreply.github.com>
2021-08-30 18:51:14 +1000

    Also toggle statusline Neovim LSP indicator

--- vim/lua/lsp-diagnostics.lua
+++ vim/lua/lsp-diagnostics.lua
@@ -9,11 +9,13 @@ local M = {}
 M.toggle = function()
   if vim.g.lspDiagnosticsVisible then
     vim.g.lspDiagnosticsVisible = false
+    vim.g.moonflyWithNvimLspIndicator = false
     vim.lsp.diagnostic.clear(0)
     vim.lsp.handlers["textDocument/publishDiagnostics"] = handlers.no_diagnostics
     print('Diagnostics are hidden')
   else
     vim.g.lspDiagnosticsVisible = true
+    vim.g.moonflyWithNvimLspIndicator = true
     vim.lsp.handlers["textDocument/publishDiagnostics"] = handlers.diagnostics
     print('Diagnostics are visible')
   end
