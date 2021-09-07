3c85926e6099c9fbf8e717a732b2d5844c8afd63
Dennis B <bluz71@users.noreply.github.com>
2021-09-07 17:37:26 +1000

    Limit pears.nvim only to end-of-line expansion

--- vim/lua/pears-config.lua
+++ vim/lua/pears-config.lua
@@ -2,34 +2,45 @@ local pears = require('pears')
 local R = require('pears.rule')
 
 pears.setup(function(conf)
+  conf.pair('{', {
+    close = '}',
+    should_expand = R.match_next('$')
+  })
+  conf.pair('[', {
+    close = ']',
+    should_expand = R.match_next('$')
+  })
+  conf.pair('(', {
+    close = ')',
+    should_expand = R.match_next('$')
+  })
+  conf.pair('"', {
+    close = '"',
+    should_expand = R.match_next('$')
+  })
+  conf.pair('"""', nil)
+  conf.pair("'", {
+    close = "'",
+    should_expand = R.match_next('$')
+  })
+  conf.pair("'''", nil)
+  conf.pair('`', {
+    close = '`',
+    should_expand = R.match_next('$')
+  })
+  conf.pair('```', {
+    close = '```',
+    filetypes = {'markdown'},
+    should_expand = R.match_next('$')
+  })
   conf.pair('<%', {
     close = '%>',
     filetypes = {'eruby'}
   })
   conf.pair('|', {
     close = '|',
-    filetypes = {'ruby'}
-  })
-  conf.pair('"', {
-    close = '"',
-    filetypes = {
-      exclude = {'vim'}
-    },
-    -- Don't expand quote before/after an alphanumeric or before/after another
-    -- quote.
-    should_expand = R.all_of(
-      R.not_(R.start_of_context('[%w"]')),
-      R.not_(R.match_next("[%w']"))
-    )
-  })
-  conf.pair("'", {
-    close = "'",
-    -- Don't expand quote before/after an alphanumeric or before/after another
-    -- quote.
-    should_expand = R.all_of(
-      R.not_(R.start_of_context("[%w']")),
-      R.not_(R.match_next('[%w"]'))
-    )
+    filetypes = {'ruby'},
+    should_expand = R.match_next("$")
   })
   conf.expand_on_enter(false)
   conf.remove_pair_on_inner_backspace(false)
