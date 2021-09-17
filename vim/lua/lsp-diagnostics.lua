095cd0f1ed8579731363b7f716e13ff3e8b8fb45
Dennis B <bluz71@users.noreply.github.com>
2021-09-17 15:36:30 +1000

    Neovim diagnostic API will be changing

--- vim/lua/lsp-diagnostics.lua
+++ vim/lua/lsp-diagnostics.lua
@@ -5,10 +5,12 @@ local M = {}
 -- Toggleable diagnostics function (see ~/dotfiles/vim/autoload/linting.vim).
 M.toggle = function()
   if vim.g.lintingActive then
-    vim.lsp.diagnostic.clear(0)
+    vim.lsp.diagnostic.clear(0) -- OLD API
+    -- vim.diagnostic.hide()    -- NEW API
     vim.lsp.handlers["textDocument/publishDiagnostics"] = handlers.no_diagnostics
   else
     vim.lsp.handlers["textDocument/publishDiagnostics"] = handlers.diagnostics
+    -- vim.diagnostic.show() -- NEW API
   end
 end
 
