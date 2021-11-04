d2ea3fc663f593774da79b0da061d75c91b3d74e
Dennis B <bluz71@users.noreply.github.com>
2021-11-04 12:52:54 +1100

    Extended nvim-autopairs disabled filetypes

--- vim/lua/plugin/autopairs.lua
+++ vim/lua/plugin/autopairs.lua
@@ -3,7 +3,7 @@ local Rule = require('nvim-autopairs.rule')
 local cond = require('nvim-autopairs.conds')
 
 npairs.setup({
-  disable_filetype = {'text', 'TelescopePrompt'},
+  disable_filetype = {'text', 'txt', 'TelescopePrompt'},
   fast_wrap = {}
 })
 
