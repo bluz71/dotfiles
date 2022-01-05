f6b8a5458e6ca5812fbd7523c1a70bb4f8190422
Dennis B <bluz71@users.noreply.github.com>
2022-01-05 15:19:07 +1100

    Treesitter textobjects are also superslow with Ruby
    Sigh :(

--- vim/lua/plugin/treesitter.lua
+++ vim/lua/plugin/treesitter.lua
@@ -19,7 +19,7 @@ treesitter.setup({
   textobjects = {
     select = {
       enable = true,
-      disable = { "dart" }, -- Temporary fix, see: https://is.gd/E00YDa
+      disable = { "dart", "ruby" }, -- Temporary fix, see: https://is.gd/E00YDa
       lookahead = true,
       keymaps = {
         ["am"] = "@function.outer",
@@ -28,7 +28,7 @@ treesitter.setup({
     },
     move = {
       enable = true,
-      disable = { "dart" }, -- Temporary fix, see: https://is.gd/E00YDa
+      disable = { "dart", "ruby" }, -- Temporary fix, see: https://is.gd/E00YDa
       set_jumps = true,
       goto_next_start = {
         ["]m"] = "@function.outer",
