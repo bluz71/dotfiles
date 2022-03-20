4e81eb0012025510e654059b48855073fe708ff1
Dennis B <bluz71@users.noreply.github.com>
2022-03-20 15:55:48 +1100

    Fatten up gitsign delete indicator

--- vim/lua/plugin/gitsigns.lua
+++ vim/lua/plugin/gitsigns.lua
@@ -9,8 +9,8 @@ gitsigns.setup({
   signs = {
     add = { hl = "GitSignsAdd", text = "│" },
     change = { hl = "GitSignsChange", text = "│" },
-    delete = { hl = "GitSignsDelete", text = "│" },
-    topdelete = { hl = "GitSignsDelete", text = "│" },
+    delete = { hl = "GitSignsDelete", text = "┃" },
+    topdelete = { hl = "GitSignsDelete", text = "┃" },
     changedelete = { hl = "GitSignsChangeDelete", text = "┃" },
   },
   keymaps = {
