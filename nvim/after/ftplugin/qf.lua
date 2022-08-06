6aae2a6cbfe14211a5097fcce2df4036466f8adb
Dennis B <bluz71@users.noreply.github.com>
2022-08-06 15:28:44 +1000

    Minor fixes

--- nvim/after/ftplugin/qf.lua
+++ nvim/after/ftplugin/qf.lua
@@ -1,6 +1,6 @@
 local opt_local = vim.opt_local
 
-opt_local.colorcolumn = 0
+opt_local.colorcolumn = "0"
 opt_local.cursorline = true
 if vim.fn.exists("&cursorlineopt") == 1 then
   opt_local.cursorlineopt = "both"
