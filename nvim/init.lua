242be0c7f21d4f601305b1f16974f855175043e6
Dennis B <bluz71@users.noreply.github.com>
2022-08-08 15:50:06 +1000

    Switch from init.vim to init.lua

---
+++ nvim/init.lua
@@ -0,0 +1,44 @@
+local cmd = vim.cmd
+local fn = vim.fn
+
+-------------------------------------------------------------
+-- OPTIONS
+-------------------------------------------------------------
+require("custom.options")
+
+
+-------------------------------------------------------------
+-- FUNCTIONS
+--
+-- ~/dotfiles/nvim/autoload - custom functions
+-------------------------------------------------------------
+
+
+-------------------------------------------------------------
+-- MAPPINGS
+-------------------------------------------------------------
+require("custom.mappings")
+cmd([[runtime custom/mappings.vim]])
+
+
+-------------------------------------------------------------
+-- PLUGINS
+-------------------------------------------------------------
+require("plugin.packer")
+
+-------------------------------------------------------------
+-- AUTOCMDS
+-------------------------------------------------------------
+cmd([[runtime custom/autocmds.vim]])
+    -- ~/dotfiles/vim/ftplugin       - file type options, mappings
+    -- ~/dotfiles/vim/after/ftplugin - file type overrides
+
+
+-------------------------------------------------------------
+-- COLOR SCHEME
+-------------------------------------------------------------
+if fn.filereadable(fn.expand("~/.colorscheme")) == 1 then
+    cmd("exec 'colorscheme ' . readfile(expand('~/.colorscheme'))[0]")
+else
+    cmd.colorscheme("moonfly")
+end
