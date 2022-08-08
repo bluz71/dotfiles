20084a8b6f06c812b83a88daeccb194fe031d2f0
Dennis B <bluz71@users.noreply.github.com>
2022-08-08 15:33:45 +1000

    Further Neovim Vimscript --> Lua mappings work

--- nvim/lua/custom/mappings.lua
+++ nvim/lua/custom/mappings.lua
@@ -98,3 +98,49 @@ map("n", "k", "v:count ? (v:count > 5 ? \"m'\" . v:count : '') . 'k' : 'gk'", ex
 map("n", "<Leader><Tab>", ":buffer<Space><Tab>")
 -- Alternate the last two files.
 map("n", "<Backspace>", "<C-^>")
+
+-------------------------------
+-- Paired mappings
+-------------------------------
+map("n", "{", "{zz")
+map("n", "}", "}zz")
+map("n", "n", "nzz")
+map("n", "N", "Nzz")
+map("n", "[c", "[czz")
+map("n", "]c", "]czz")
+map("n", "[j", "<C-o>zz")
+map("n", "]j", "<C-i>zz")
+map("n", "[s", "[szz")
+map("n", "]s", "]szz")
+map("n", "[z", "zH")
+map("n", "]z", "zL")
+
+-------------------------------
+-- Scroll mappings
+-------------------------------
+map("n", "<C-Up>", "<C-e>")
+map("n", "<C-Down>", "<C-y>")
+
+-------------------------------
+-- Window managment mappings
+-------------------------------
+map("n", "<Leader>s", "<C-w>s")
+map("n", "<Leader>v", "<C-w>v")
+map("n", "<Leader>-", ":botright new<CR><C-w>=", silent_opts)
+map("n", "<Leader>|", ":botright vnew<CR><C-w>=", silent_opts)
+map("n", "<Leader>t", ":$tabnew<CR>", silent_opts)
+map("n", "<Leader>q", "<C-w>c")
+map("n", "<Leader>Q", ":tabclose<CR>", silent_opts)
+map("n", "<Leader>c", ":cclose<CR>:lclose<CR>:pclose<CR>", silent_opts)
+map("n", "<Leader>1", "1gt")
+map("n", "<Leader>2", "2gt")
+map("n", "<Leader>3", "3gt")
+map("n", "<Leader>4", "4gt")
+map("n", "<Leader>5", "5gt")
+map("n", "<Leader>6", "6gt")
+map("n", "<Leader>7", "7gt")
+map("n", "<Leader>8", "8gt")
+map("n", "<Leader>9", "9gt")
+map("n", "<Leader>=", "<C-w>=")
+map("n", "<Leader>r", "<C-w>r")
+map("n", "<Leader>T", ":botright new<CR><C-w>=:terminal<CR>", silent_opts)
