2a963c8f7505e980dce4bc02ecb29c46c737521e
Dennis B <bluz71@users.noreply.github.com>
2022-08-11 16:11:23 +1000

    Fern hacks

--- nvim/plugin/fern.lua
+++ nvim/plugin/fern.lua
@@ -1,7 +1,7 @@
-local g = vim.g
-local map = vim.keymap.set
 local augroup = vim.api.nvim_create_augroup
 local autocmd = vim.api.nvim_create_autocmd
+local g = vim.g
+local map = vim.keymap.set
 
 g["fern#disable_default_mappings"] = 1
 g["fern#disable_drawer_smart_quit"] = 1
@@ -17,11 +17,24 @@ map("n", "<Leader>d", ":Fern . -drawer -width=35 -toggle<CR><C-w>=", opts)
 map("n", "<Leader>f", ":Fern . -drawer -reveal=% -width=35<CR><C-w>=", opts)
 map("n", "<Leader>.", ":Fern %:h -drawer -width=35<CR><C-w>=", opts)
 
+-- Open directory tree path.
+vim.cmd([[
+  function! FernDirectoryOpen() abort
+      let l:path = expand('%:p')
+
+      if !isdirectory(l:path)
+          return
+      endif
+
+      execute 'Fern ' . fnameescape(path)
+  endfunction
+]])
+
 -- Let fern handle directory paths instead of Netrw.
 local fern_plugin_events = augroup("FernPluginEvents", {})
 autocmd("BufEnter", {
   pattern = "*",
   nested = true,
-  command = "call tree#Open()",
+  command = "call FernDirectoryOpen()",
   group = fern_plugin_events
 })
