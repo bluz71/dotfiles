7f4500c9c70c8ae6a5d785e215f58ad76e8272bb
Dennis B <bluz71@users.noreply.github.com>
2022-08-15 16:45:24 +1000

    Neovim 'gf' fix for Lua files

--- nvim/after/ftplugin/lua.lua
+++ nvim/after/ftplugin/lua.lua
@@ -1,1 +1,8 @@
-vim.opt_local.formatoptions = "tcqj"
+local fn = vim.fn
+local opt_local = vim.opt_local
+
+opt_local.formatoptions = "tcqj"
+-- Options for 'gf' to successfully operate on local Lua 'require' statements.
+opt_local.includeexpr = "substitute(v:fname, '\\.', '/', 'g')"
+opt_local.path:prepend(fn.stdpath("config") .. "/lua")
+opt_local.suffixesadd:prepend(".lua")
