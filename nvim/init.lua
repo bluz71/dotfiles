7cf5caa30a94ebb24b2295911660ec5851270440
Dennis B <bluz71@users.noreply.github.com>
2022-08-18 17:58:55 +1000

    Style fix

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,14 +1,14 @@
 local cmd = vim.cmd
 local fn = vim.fn
 
-require("custom.options")  -- Options
+require("custom.options") -- Options
 require("custom.mappings") -- Mappings
-require("custom.packer")   -- Plugin Manager
+require("custom.packer") -- Plugin Manager
 require("custom.autocmds") -- Auto-commands.
 
 -- Colorscheme, load from a dotfile if it exists.
 if fn.filereadable(fn.expand("~/.colorscheme")) == 1 then
-  cmd([[colorscheme ]] .. fn.readfile(fn.expand('~/.colorscheme'))[1])
+  cmd([[colorscheme ]] .. fn.readfile(fn.expand("~/.colorscheme"))[1])
 else
   cmd([[colorscheme moonfly]])
 end
