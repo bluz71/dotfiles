1567c262c4388d94be109b37043c8cab00ab5dfb
Dennis B <bluz71@users.noreply.github.com>
2022-08-20 17:51:38 +1000

    Simplified Neovim diagnostic configuration

--- nvim/plugin/diagnostic.lua
+++ nvim/plugin/diagnostic.lua
@@ -1,15 +1,33 @@
-local diagnostic_style = require("util.diagnostic-style")
 local cmd = vim.cmd
+local sign_define = vim.fn.sign_define
+
+-- Diagnostic symbols for display in the sign column.
+sign_define("DiagnosticSignError", { text = "▶", texthl = "DiagnosticSignError" })
+sign_define("DiagnosticSignWarn", { text = "△", texthl = "DiagnosticSignWarn" })
 
 -- Preferred global diagnostic style for 'vim.diagnostic.*' displaying
 -- functions.
-vim.diagnostic.config(diagnostic_style.config())
-
--- Diagnostic symbols for display in the sign column.
-cmd([[
-  sign define DiagnosticSignError text=▶ texthl=DiagnosticSignError
-  sign define DiagnosticSignWarn  text=△ texthl=DiagnosticSignWarn
-]])
+vim.diagnostic.config({
+  float = {
+    show_header = false,
+    source = "if_many",
+    border = "single",
+  },
+  severity_sort = true,
+  signs = {
+    severity = {
+      min = vim.diagnostic.severity.WARN,
+    },
+  },
+  update_in_insert = false,
+  underline = false,
+  virtual_text = {
+    spacing = 2,
+    severity = {
+      min = vim.diagnostic.severity.WARN,
+    },
+  },
+})
 
 local map = vim.keymap.set
 map("n", "]d", function()
@@ -21,5 +39,5 @@ map("n", "[d", function()
   cmd([[normal zz]])
 end)
 map("n", "'d", function()
-  vim.diagnostic.open_float(0, {scope = "line"})
+  vim.diagnostic.open_float(0, { scope = "line" })
 end)
