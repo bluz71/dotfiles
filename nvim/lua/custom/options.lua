6cc2dbcddd4433174b99a4164729660b4ae760ba
Dennis B <bluz71@users.noreply.github.com>
2022-09-09 17:33:53 +1000

    Neovim diff style

--- nvim/lua/custom/options.lua
+++ nvim/lua/custom/options.lua
@@ -32,7 +32,8 @@ opt.exrc = false -- Disable reading of working directory vimrc files
 opt.fillchars = {
   horiz = "━", horizup = "┻", horizdown = "┳", vert = "┃", vertleft = "┫",
   vertright = "┣", verthoriz = "╋",
-  foldclose = "▷", foldopen = "▼", foldsep = "┃"
+  foldclose = "▷", foldopen = "▼", foldsep = "┃",
+  diff = "╌"
 }
 opt.foldlevelstart = 20
 opt.foldmethod = "indent" -- Simple and fast
