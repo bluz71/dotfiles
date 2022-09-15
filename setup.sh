62cf55bcf0f2d365ae91bef697be60075c41207b
Dennis B <bluz71@users.noreply.github.com>
2022-09-15 17:33:54 +1000

    Move linter configs to a subdirectory

--- setup.sh
+++ setup.sh
@@ -4,18 +4,17 @@ git clone --depth 1 https://github.com/bluz71/bash-seafly-prompt ~/.bash-seafly-
 
 cd ~
 ln -s dotfiles/bash_profile .bash_profile
-ln -s dotfiles/bashrc       .bashrc
-ln -s dotfiles/gitconfig    .gitconfig
-ln -s dotfiles/gvimrc       .gvimrc
-ln -s dotfiles/inputrc      .inputrc
-ln -s dotfiles/irbrc        .irbrc
-ln -s dotfiles/mdlrc        .mdlrc
-ln -s dotfiles/pryrc        .pryrc
-ln -s dotfiles/tmux.conf    .tmux.conf
-ln -s dotfiles/vim/         .vim
-ln -s dotfiles/vimrc        .vimrc
+ln -s dotfiles/bashrc .bashrc
+ln -s dotfiles/gitconfig .gitconfig
+ln -s dotfiles/gvimrc .gvimrc
+ln -s dotfiles/inputrc .inputrc
+ln -s dotfiles/irbrc .irbrc
+ln -s dotfiles/pryrc .pryrc
+ln -s dotfiles/tmux.conf .tmux.conf
+ln -s dotfiles/vim/ .vim
+ln -s dotfiles/vimrc .vimrc
 
-ln -s ~/dotfiles/nvim/       .config/nvim
+ln -s ~/dotfiles/nvim/ .config/nvim
 
 mkdir -p .local/share/bash-completion
 ln -s ~/dotfiles/completions .local/share/bash-completion
@@ -23,8 +22,10 @@ ln -s ~/dotfiles/completions .local/share/bash-completion
 mkdir -p scripts
 ln -s ~/dotfiles/git-bvv scripts/git-bvv
 
+ln -s dotfiles/linter_configs/mdlrc .mdlrc
+
 mkdir -p .config/yamllint
-ln -s ~/dotfiles/yamllint .config/yamllint/config
+ln -s ~/dotfiles/linter_configs/yamllint .config/yamllint/config
 
 mkdir -p .config/alacritty
 
