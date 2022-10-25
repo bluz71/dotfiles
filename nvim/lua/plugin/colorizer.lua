f7b53919e3e437d23ebe84446d634d7e330fb006
Dennis B <bluz71@users.noreply.github.com>
2022-10-25 17:39:52 +1100

    Colorize Lua files

--- nvim/lua/plugin/colorizer.lua
+++ nvim/lua/plugin/colorizer.lua
@@ -1,7 +1,7 @@
 local colorizer = require("colorizer")
 
 colorizer.setup({
-  filetypes = { "css", "markdown", "scss", "vim", "yaml" },
+  filetypes = { "css", "lua", "markdown", "scss", "vim", "yaml" },
   user_default_options = {
     names = false,
   }
