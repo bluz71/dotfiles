f5089c36ed06508d3d0a29a708226749884104e8
Dennis B <bluz71@users.noreply.github.com>
2023-02-06 14:19:42 +1100

    Fix broken Fern on Neovim issue
    Launching Neovim with a directory path, e.g. `nvim .`, was not working.
    Now fixed.

--- nvim/lua/config/fern.lua
+++ nvim/lua/config/fern.lua
@@ -1,7 +1,3 @@
-local augroup = vim.api.nvim_create_augroup
-local autocmd = vim.api.nvim_create_autocmd
-local cmd = vim.cmd
-local fn = vim.fn
 local g = vim.g
 local map = vim.keymap.set
 
@@ -19,23 +15,3 @@ local opts = { silent = true }
 map("n", "<Leader>d", ":Fern . -drawer -width=35 -toggle<CR><C-w>=", opts)
 map("n", "<Leader>f", ":Fern . -drawer -reveal=% -width=35<CR><C-w>=", opts)
 map("n", "<Leader>.", ":Fern %:h -drawer -width=35<CR><C-w>=", opts)
-
--- Open directory tree path.
-local fern_directory_open = function()
-  local path = fn.expand("%:p")
-
-  if fn.isdirectory(path) == 0 then
-    return
-  end
-
-  cmd([[execute 'Fern ]] .. fn.fnameescape(path) .. "'")
-end
-
--- Let fern handle directory paths instead of Netrw.
-local fern_plugin_events = augroup("FernPluginEvents", {})
-autocmd("BufEnter", {
-  pattern = "*",
-  nested = true,
-  callback = fern_directory_open,
-  group = fern_plugin_events
-})
