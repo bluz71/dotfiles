1935718edd5ceca18f564e021772e2933e4671b2
Dennis B <bluz71@users.noreply.github.com>
2023-02-07 14:36:45 +1100

    Add Tree-sitter support for: regex, markdown & markdown_inline

--- nvim/lua/config/treesitter.lua
+++ nvim/lua/config/treesitter.lua
@@ -3,8 +3,9 @@ local treesitter = require("nvim-treesitter.configs")
 treesitter.setup({
   ensure_installed = {
     "bash", "c", "comment", "cpp", "css", "dart", "elixir", "go", "help",
-    "html", "java", "javascript", "lua", "python", "ruby", "rust", "scala",
-    "scss", "toml", "typescript", "vim", "vue"
+    "html", "java", "javascript", "lua", "markdown", "markdown_inline",
+    "python", "regex", "ruby", "rust", "scala", "scss", "toml", "typescript",
+    "vim", "vue"
   },
   highlight = {
     enable = true,
