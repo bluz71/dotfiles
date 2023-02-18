e02818a579f96a74ee0672af6ccacc40b0536ee1
Dennis B <bluz71@users.noreply.github.com>
2023-02-18 16:14:19 +1100

    Remove print message

--- nvim/lua/config/treesitter.lua
+++ nvim/lua/config/treesitter.lua
@@ -14,7 +14,6 @@ treesitter.setup({
       local max_filesize = 100 * 1024 -- 100 KB
       local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
       if ok and stats and stats.size > max_filesize then
-        print("(Tree-sitter) DISABLED, file too large")
         return true
       end
     end,
