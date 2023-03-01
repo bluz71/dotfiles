ced85254e5e69c5a8875e8b36fa9fe2e9f412a1b
Dennis B <bluz71@users.noreply.github.com>
2023-03-01 15:18:19 +1100

    Update Neovim plugins

--- nvim/lua/custom/lazy.lua
+++ nvim/lua/custom/lazy.lua
@@ -126,7 +126,6 @@ lazy.setup({
   -----------------------------
   {
     "nvim-telescope/telescope.nvim",
-    branch = "0.1.x",
     dependencies = {
       "nvim-lua/plenary.nvim",
       { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
