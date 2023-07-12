bea1e54342b6295eb21160d0795fd0a93e910dda
Dennis B <bluz71@users.noreply.github.com>
2023-07-12 14:53:39 +1000

    Use nicer ../ abbreviation for parent directory expansion

--- fish/functions/parent_directory.fish
+++ fish/functions/parent_directory.fish
@@ -1,4 +1,4 @@
-function parent_directory --description 'Parent directory for ,, abbreviation'
+function parent_directory --description 'Parent directory for ../ abbreviation'
     echo (dirname (pwd))
 end
 
