fdca8da016bfb97ed7cd15c1703dd50f854f183a
Dennis B <bluz71@users.noreply.github.com>
2023-08-19 13:32:28 +1000

    Fix cwd function in Fish shell

--- fish/functions/cwd.fish
+++ fish/functions/cwd.fish
@@ -5,7 +5,7 @@ function cwd --description 'Copy working directory'
         echo -n (echo $PWD | sed "s|^$HOME|~|") | tr -d "\r\n" | pbcopy
     end
     # Also copy current directory to a tmux paste buffer if tmux is active.
-    if test -n $TMUX
+    if test -n "$TMUX"
         echo -n (echo $PWD | sed "s|^$HOME|~|") | tr -d "\r\n" | tmux load-buffer -
     end
 end
