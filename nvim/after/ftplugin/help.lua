f6ec94ec7d1c06b97759f153c4909b8e06c12958
Dennis B <bluz71@users.noreply.github.com>
2023-09-03 13:36:52 +1000

    Remove `statuscolumn` workaround (I don't use `statuscolumn`)

--- nvim/after/ftplugin/help.lua
+++ nvim/after/ftplugin/help.lua
@@ -3,6 +3,3 @@ local opt_local = vim.opt_local
 opt_local.number = false
 opt_local.relativenumber = false
 opt_local.signcolumn = "no"
-if vim.fn.has("nvim-0.9") == 1 then
-  opt_local.statuscolumn = nil
-end
