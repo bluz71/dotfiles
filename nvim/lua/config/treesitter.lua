6dbe32993ff8eb242e7b8061c2be64bc668ec3c8
Dennis B <bluz71@users.noreply.github.com>
2023-09-30 20:32:50 +1000

    Neovim ships: c, lua, vim & vimdoc Tree-sitter parsers

--- nvim/lua/config/treesitter.lua
+++ nvim/lua/config/treesitter.lua
@@ -2,12 +2,16 @@ local treesitter = require("nvim-treesitter.configs")
 local buffer = require("util.buffer")
 
 treesitter.setup({
+  -- No need to ensure_installed the following parsers.
+  --
+  -- Neovim 0.9 ships: c, lua, vim & vimdoc parsers
+  -- Neovim 0.10 ships: bash, c, lua, markdown, markdown_inline, python, vim &
+  --                    vimdoc parsers
   ensure_installed = {
-    "astro", "bash", "c", "clojure", "cpp", "css", "dart", "elixir", "elm",
-    "embedded_template", "go", "haskell", "html", "java", "javascript",
-    "julia", "lua", "markdown", "markdown_inline", "php", "python", "r",
-    "regex", "ruby", "rust", "scala", "scss", "svelte", "toml", "tsx",
-    "typescript", "vim", "vimdoc", "vue",
+    "astro", "bash", "clojure", "cpp", "css", "dart", "elixir", "elm",
+    "embedded_template", "go", "haskell", "html", "java", "javascript", "julia",
+    "markdown", "markdown_inline", "php", "python", "r", "regex", "ruby",
+    "rust", "scala", "scss", "svelte", "toml", "tsx", "typescript", "vue",
   },
   highlight = {
     enable = true,
@@ -23,13 +27,8 @@ treesitter.setup({
   indent = {
     enable = true,
     disable = function(lang, buf)
-      if
-        lang == "html"
-        or lang == "ruby"
-        or lang == "rust"
-        or buffer.is_large(buf)
-      then
-        -- Disable indent for certain filetypes AND also for large files.
+      if lang == "html" or lang == "ruby" or lang == "rust" or buffer.is_large(buf) then
+        -- Disable indent for certain filetypes & large files.
         return true
       end
     end,
