127a8d3e77768ab3a0d726de8e28922e23cd7c7a
Dennis B <bluz71@users.noreply.github.com>
2024-11-09 17:46:19 +1100

    Change colorizer virtualtext symbol

--- nvim/lua/config/colorizer.lua
+++ nvim/lua/config/colorizer.lua
@@ -3,12 +3,17 @@ local colorizer = require("colorizer")
 colorizer.setup({
   filetypes = {
     "css",
-    eruby = { mode = "virtualtext", names = true, tailwind = true, virtualtext_inline = true },
-    html = { mode = "virtualtext", names = true, tailwind = true, virtualtext_inline = true },
+    eruby = {
+      mode = "virtualtext", names = true, tailwind = true, virtualtext_inline = true,
+    },
+    html = {
+      mode = "virtualtext", names = true, tailwind = true, virtualtext_inline = true,
+    },
     "lua", "markdown", "text", "tmux", "toml", "txt", "vim", "yaml",
   },
   user_default_options = {
     mode = "background",
     names = false,
+    virtualtext = "▮",
   },
 })
