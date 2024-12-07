b5d032f406eb46e1b3a3d28f0f70debdd1a5f5e4
Dennis B <bluz71@users.noreply.github.com>
2024-12-07 15:58:15 +1100

    docker and pnpm completions for fish shell

--- fish/completions/pnpm.fish
+++ fish/completions/pnpm.fish
@@ -4,7 +4,7 @@ function _pnpm_completion
   set cursor (commandline -C)
   set words (count $cmd)
 
-  set completions (eval env DEBUG=\"" \"" COMP_CWORD=\""$words\"" COMP_LINE=\""$cmd \"" COMP_POINT=\""$cursor\"" pnpm completion -- $cmd)
+  set completions (eval env DEBUG=\"" \"" COMP_CWORD=\""$words\"" COMP_LINE=\""$cmd \"" COMP_POINT=\""$cursor\"" SHELL=fish pnpm completion-server -- $cmd)
 
   if [ "$completions" = "__tabtab_complete_files__" ]
     set -l matches (commandline -ct)*
@@ -20,3 +20,4 @@ end
 
 complete -f -d 'pnpm' -c pnpm -a "(_pnpm_completion)"
 ###-end-pnpm-completion-###
+
