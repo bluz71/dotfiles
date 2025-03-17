dfd8a7b20a3c791c9664a5c52ca5671f3fc9449f
Dennis B <bluz71@users.noreply.github.com>
2025-03-17 18:37:59 +1100

    Notes about Neovim 0.11

--- nvim/lua/util/lsp-handlers.lua
+++ nvim/lua/util/lsp-handlers.lua
@@ -7,9 +7,21 @@ M.hover = lsp.with(lsp.handlers.hover, {
   border = "single",
 })
 
+-- Neovim 0.11
+-- local hover = vim.lsp.buf.hover
+-- vim.lsp.buf.hover = function()
+--   hover({ border = "single" })
+-- end
+
 -- Also add borders to signature help windows.
 M.signature_help = lsp.with(lsp.handlers.signature_help, {
   border = "single",
 })
+
+-- Neovim 0.11
+-- local signature_help = vim.lsp.buf.signature_help
+-- vim.lsp.buf.signature_help = function()
+--   signature_help({ border = "single" })
+-- end
 
 return M
