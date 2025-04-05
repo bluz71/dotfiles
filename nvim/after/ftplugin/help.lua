18b8989ff73652f46d91b785357aeac2400f0393
Dennis B <bluz71@users.noreply.github.com>
2025-04-05 16:28:17 +1100

    Tweak Neovim help filetype styling

--- nvim/after/ftplugin/help.lua
+++ nvim/after/ftplugin/help.lua
@@ -1,7 +1,9 @@
 local opt_local = vim.opt_local
 
-opt_local.number = false
-opt_local.relativenumber = false
+opt_local.number = true
+opt_local.numberwidth = 4
+opt_local.relativenumber = true
 opt_local.signcolumn = "no"
+opt_local.statuscolumn = ""
 
 vim.b.snacks_indent = false
