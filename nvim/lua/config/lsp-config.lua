3adbe4f4b75c9bf7817eaf08e9e9fc8e4deae52f
Dennis B <bluz71@users.noreply.github.com>
2025-09-15 18:16:57 +1000

    Switch over to the new Neovim LSP API (not the old nvim-lspconfig API)
    The old nvim-lspconfig API is deprecated and will one day be removed.

--- nvim/lua/config/lsp-config.lua
+++ nvim/lua/config/lsp-config.lua
@@ -1,7 +1,13 @@
+local augroup = vim.api.nvim_create_augroup
+local autocmd = vim.api.nvim_create_autocmd
+local bo = vim.bo
+local buf_get_name = vim.api.nvim_buf_get_name
 local cmd = vim.cmd
+local get_client_by_id = vim.lsp.get_client_by_id
 local lsp = vim.lsp
+local lsp_config = vim.lsp.config
+local lsp_enable = vim.lsp.enable
 local map = vim.keymap.set
-local opt_local = vim.opt_local
 
 -- Do not load up LSP when in diff mode.
 if vim.opt.diff:get() or vim.env.NVIM_GIT_DIFF == "1" then
@@ -9,84 +15,80 @@ if vim.opt.diff:get() or vim.env.NVIM_GIT_DIFF == "1" then
 end
 
 local nvim_lsp = require("lspconfig")
-local nvim_lsp_windows = require("lspconfig.ui.windows")
 local buffer = require("util.buffer")
 local lsp_capabilities = require("util.lsp-capabilities")
 
-------------------------------
--- The On Attach Functions  --
-------------------------------
+---------------------------
+-- Attach Functionality  --
+---------------------------
 
--- Default on attach function.
-local lsp_on_attach = function()
-  -- Disable LSP for files larger than 100KB.
-  if buffer.is_large(0) then
-    print("(LSP) DISABLED, file too large")
-    cmd([[LspStop]])
-    return
-  end
+autocmd("LspAttach", {
+  callback = function(args)
+    local client = get_client_by_id(args.data.client_id)
 
-  -- Default LSP mappings provided by Neovim itself:
-  --   K        - Hover
-  --   grr      - references
-  --   grn      - rename
-  --   gra      - code action
-  --
-  -- Custom mappings:
-  map("n", "gd", lsp.buf.definition, { buffer = true })
-  -- Note, in my Alacritty terminal config I have 'Control-c' re-mapped to 'Control-k', hence this
-  -- mapping actually is 'Control-k'.
-  map("i", "<C-c>", lsp.buf.signature_help, { buffer = true })
+    -- Disable LSP for files larger than 100KB.
+    if buffer.is_large(0) then
+      print("(LSP) DISABLED, file too large")
+      cmd([[LspStop]])
+      return
+    end
 
-  -- Note, LSP formatting will be handled by the conform.nvim plugin.
+    -- Default LSP mappings provided by Neovim itself:
+    --   K        - Hover
+    --   grr      - references
+    --   grn      - rename
+    --   gra      - code action
+    --
+    -- Custom mappings:
+    map("n", "gd", lsp.buf.definition, { buffer = true })
+    -- Note, in my Alacritty terminal config I have 'Control-c' re-mapped to 'Control-k', hence this
+    -- mapping actually is 'Control-k'.
+    map("i", "<C-c>", lsp.buf.signature_help, { buffer = true })
 
-  -- Disable Neovim LSP-set 'omnifunc' and 'formatexpr' options; these options
-  -- cause problems with my custom-completion mapping along with the 'gq'
-  -- command.
-  opt_local.omnifunc = ""
-  opt_local.formatexpr = ""
-end
+    -- Disable LSP formatting for certain Language Servers where Conform, running a command line
+    -- formatter, will instead be used.
+    if
+      client.name == "cssls"
+      or client.name == "eslint"
+      or client.name == "html"
+      or client.name == "ruby_lsp"
+      or client.name == "ts_ls"
+      or client.name == "tailwindcss"
+    then
+      client.server_capabilities.documentFormattingProvider = false
+      client.server_capabilities.documentRangeFormattingProvider = false
+    end
 
--- Custom on attach function which disables LSP formatting where Conform running command line
--- formatter will instead be used.
-local lsp_on_attach_no_formatting = function(client)
-  client.server_capabilities.documentFormattingProvider = false
-  client.server_capabilities.documentRangeFormattingProvider = false
+    -- Tailwind LSP trigger characters are annoying, disable them.
+    --
+    -- Note, to list current trigger characters run this command:
+    --   :lua print(vim.inspect(vim.lsp.buf_get_clients()[1].server_capabilities.completionProvider.triggerCharacters))
+    if client.name == "tailwindcss" then
+      client.server_capabilities.completionProvider.triggerCharacters = {}
+    end
 
-  lsp_on_attach()
-end
+    -- Disable Neovim LSP-set 'omnifunc' and 'formatexpr' options; these options cause problems with
+    -- my custom-completion mapping along with the 'gq' command.
+    bo.omnifunc = ""
+    bo.formatexpr = ""
+  end,
+  group = augroup("CustomLspEvent", {}),
+})
 
--- Custom on attach function which disable LSP semantic highlighting.
--- local lsp_on_attach_no_semantic_highlights = function(client)
---   client.server_capabilities.semanticTokensProvider = nil
---
---   lsp_on_attach()
--- end
-
--- Tailwind LSP on attach which disables formatting and trigger characters.
-local tailwind_lsp_on_attach = function(client)
-  client.server_capabilities.documentFormattingProvider = false
-  client.server_capabilities.documentRangeFormattingProvider = false
-  -- Tailwind LSP trigger characters are annoying, disable them.
-  -- Note, to list current trigger characters run this command:
-  --   :lua print(vim.inspect(vim.lsp.buf_get_clients()[1].server_capabilities.completionProvider.triggerCharacters))
-  client.server_capabilities.completionProvider.triggerCharacters = {}
-
-  lsp_on_attach()
-end
-
---------------------------
--- The Language Servers --
---------------------------
+----------------------------------------
+-- The Language Server Configurations --
+----------------------------------------
 
 -- The nvim-cmp completion plugin supports most LSP capabilities; we should notify the language
 -- servers about that.
 local capabilities = lsp_capabilities.default_capabilities()
 
-nvim_lsp.cssls.setup({
-  on_attach = lsp_on_attach_no_formatting,
+lsp_config("*", {
   capabilities = capabilities,
   flags = { debounce_text_changes = 300 },
+})
+
+lsp_config("cssls", {
   settings = {
     css = {
       lint = {
@@ -96,76 +98,32 @@ nvim_lsp.cssls.setup({
   },
 })
 
-nvim_lsp.eslint.setup({
-  on_attach = lsp_on_attach_no_formatting,
-  capabilities = capabilities,
-  filetypes = { "javascript", "typescript" },
-  flags = { debounce_text_changes = 300 },
-})
-
--- nvim_lsp.gopls.setup({
---   on_attach = lsp_on_attach,
---   capabilities = capabilities,
---   flags = { debounce_text_changes = 300 },
---   settings = { gopls = { semanticTokens = true } },
--- })
-
-nvim_lsp.html.setup({
-  on_attach = lsp_on_attach_no_formatting,
-  capabilities = capabilities,
+lsp_config("html", {
   filetypes = { "eruby", "html" },
-  flags = { debounce_text_changes = 300 },
 })
 
--- gem install ruby-lsp ruby-lsp-rails
-nvim_lsp.ruby_lsp.setup({
-  on_attach = lsp_on_attach_no_formatting, -- Use standardrb for formatting
-  capabilities = capabilities,
-  filetypes = { "ruby" },
-  flags = { debounce_text_changes = 300 },
-})
-
-nvim_lsp.rust_analyzer.setup({
-  on_attach = lsp_on_attach,
-  capabilities = capabilities,
-  flags = { debounce_text_changes = 300 },
-  root_dir = nvim_lsp.util.root_pattern("Cargo.toml"),
-  settings = {
-    ["rust-analyzer"] = {
-      checkOnSave = {
-        command = "clippy",
-        extraArgs = { "--", "-Aclippy::needless_return" },
-      },
-    },
-  },
-})
-
--- gem install standard standard-rails
-nvim_lsp.standardrb.setup({
-  on_attach = lsp_on_attach,
-  capabilities = capabilities,
+lsp_config("ruby_lsp", {
   filetypes = { "ruby" },
 })
 
-nvim_lsp.ts_ls.setup({
-  on_attach = lsp_on_attach_no_formatting,
-  capabilities = capabilities,
-  flags = { debounce_text_changes = 300 },
-  root_dir = nvim_lsp.util.root_pattern("package.json"),
+lsp_config("tailwindcss", {
+  filetypes = { "eruby", "html" },
+  root_dir = function(bufnr, on_dir)
+    on_dir(nvim_lsp.util.root_pattern("vite.config.js")(buf_get_name(bufnr)))
+  end,
 })
 
-nvim_lsp.tailwindcss.setup({
-  on_attach = tailwind_lsp_on_attach,
-  filetypes = { "astro", "eruby", "html" },
-  flags = { debounce_text_changes = 300 },
-  root_dir = nvim_lsp.util.root_pattern("vite.config.js"),
-  settings = {
-    tailwindCSS = {
-      experimental = {
-        classRegex = {
-          "(?:class:[\\s\\n]*|class=)['\"]([^'\"]+)['\"]",
-        },
-      },
-    },
-  },
+---------------------------------
+-- Enable The Language Servers --
+---------------------------------
+
+lsp_enable({
+  "cssls",
+  "eslint",
+  "html",
+  "ruby_lsp",
+  "rust_analyzer",
+  "standardrb",
+  "ts_ls",
+  "tailwindcss",
 })
