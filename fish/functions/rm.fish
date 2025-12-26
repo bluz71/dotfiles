cfbe7ae6606cf5e1017d15234051312fc834c2fc
Dennis B <bluz71@users.noreply.github.com>
2025-12-26 17:47:06 +1100

    Fix Fish scp tab-completion issue

--- fish/functions/rm.fish
+++ fish/functions/rm.fish
@@ -1,3 +1,9 @@
 function rm --description 'Interactive remove'
-    command /bin/rm -i $argv
+    # Skip interactive mode if being called on an 'scp' Fish temp file as
+    # created when executing an 'scp' tab-completion.
+    if string match -q '/tmp/fish-scp.*' -- $argv[1]
+        command /bin/rm $argv
+    else
+        command /bin/rm -i $argv
+    end
 end
