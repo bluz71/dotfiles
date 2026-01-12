b258cac13e5e9d5a07ea8c0759c887a109f8bf94
Dennis B <bluz71@users.noreply.github.com>
2026-01-12 18:18:24 +1100

    Don't bother with Elm Tree-sitter parser

--- nvim/lua/config/treesitter.lua
+++ nvim/lua/config/treesitter.lua
@@ -4,18 +4,17 @@ local augroup = vim.api.nvim_create_augroup
 local autocmd = vim.api.nvim_create_autocmd
 
 treesitter.install({
-  "astro", "bash", "c", "clojure", "cpp", "css", "dart", "diff", "elixir", "elm",
-  "embedded_template", "fish", "git_rebase", "gitcommit", "go", "haskell", "html", "java",
-  "javascript", "json", "julia", "lua", "markdown", "markdown_inline", "php", "python", "r", "rbs",
-  "regex", "ruby", "rust", "scala", "scss", "svelte", "tmux", "toml", "tsx", "typescript", "vim",
-  "vimdoc", "vue", "zig",
+  "astro", "bash", "c", "clojure", "cpp", "css", "dart", "diff", "elixir", "embedded_template",
+  "fish", "git_rebase", "gitcommit", "go", "haskell", "html", "java", "javascript", "json", "julia",
+  "lua", "markdown", "markdown_inline", "php", "python", "r", "rbs", "regex", "ruby", "rust",
+  "scala", "scss", "svelte", "tmux", "toml", "tsx", "typescript", "vim", "vimdoc", "vue", "zig",
 })
 
 autocmd("FileType", {
   pattern = {
-    "astro", "c", "clojure", "cpp", "css", "dart", "elixir", "elm", "eruby", "fish", "gitcommit",
-    "go", "haskell", "html", "java", "javascript", "json", "julia", "lua", "markdown", "php",
-    "python", "r", "ruby", "rust", "scala", "scss", "sh", "svelte", "tmux", "toml", "typescript",
+    "astro", "c", "clojure", "cpp", "css", "dart", "elixir", "eruby", "fish", "gitcommit", "go",
+    "haskell", "html", "java", "javascript", "json", "julia", "lua", "markdown", "php", "python",
+    "r", "ruby", "rust", "scala", "scss", "sh", "svelte", "tmux", "toml", "typescript",
     "typescriptreact", "vim", "vue", "zig",
   },
   callback = function(event)
