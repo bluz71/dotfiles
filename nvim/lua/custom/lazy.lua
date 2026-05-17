b433f231ff32b95f0ce5487113039027ea2b9370
Dennis B <bluz71@users.noreply.github.com>
2026-05-17 20:39:46 +1000

    Revert neo-tree to a fixed commit to avoid hidden directory jank

--- nvim/lua/custom/lazy.lua
+++ nvim/lua/custom/lazy.lua
@@ -151,6 +151,7 @@ lazy.setup({
   {
     "nvim-neo-tree/neo-tree.nvim",
     branch = "v3.x",
+    commit = "84c75e7",
     dependencies = {
       "nvim-lua/plenary.nvim",
       "MunifTanjim/nui.nvim",
