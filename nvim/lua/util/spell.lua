74b2f70a450461596afec0b324286ad809b9f012
Dennis B <bluz71@users.noreply.github.com>
2026-05-20 20:27:24 +1000

    Simplify complete configuration in Neovim

--- nvim/lua/util/spell.lua
+++ nvim/lua/util/spell.lua
@@ -5,10 +5,8 @@ local M = {}
 M.toggle = function()
   opt_local.spell = not (opt_local.spell:get()) -- Toggle 'spell' option
   if opt_local.spell:get() then
-    opt_local.complete = opt_local.complete + "kspell"
     print("(Spelling) ON")
   else
-    opt_local.complete = opt_local.complete - "kspell"
     print("(Spelling) OFF")
   end
 end
