4bbc47f96057e78ec9f83549772daa2f6e633e8a
Dennis B <bluz71@users.noreply.github.com>
2026-05-22 15:14:44 +1000

    Re-enable linefly winbar

--- nvim/after/plugin/linefly.lua
+++ nvim/after/plugin/linefly.lua
@@ -4,7 +4,7 @@ vim.g.linefly_options = {
   warning_symbol = "△",
   information_symbol = "○",
   tabline = true,
-  winbar = vim.env.NVIM_GIT_DIFF ~= "1",
+  winbar = true,
   with_lsp_status = true,
   with_search_count = true,
   with_spell_status = true,
