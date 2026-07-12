268c7a17321934ea732d38e9c03087399311f0b1
Dennis B <bluz71@users.noreply.github.com>
2026-07-12 17:41:35 +1000

    Improve abc sequence function

--- fish/functions/abc.fish
+++ fish/functions/abc.fish
@@ -1,8 +1,23 @@
 function abc --description 'Print a sequence of characters'
-    set -l s (printf '%d' "'$argv[1]")
-    set -l e (printf '%d' "'$argv[2]")
-    for i in (seq $s $e)
-        set -l oct (printf '%o' $i)
-        printf "\\$oct\n"
+    # Default to 'a' start character when only one end character is given.
+    set -l args $argv
+    if test (count $argv) -eq 1
+        set args a $argv
+    end
+
+    if test (count $args) -gt 2
+        echo "abc: too many arguments (expected 1 or 2 characters)" >&2
+        return 1
+    end
+
+    # Convert characters to ASCII codes.
+    set -l nums
+    for c in $args
+        set -a nums (printf '%d' "'"(string sub -l 1 $c))
+    end
+
+    # Convert ASCII codes back to characters.
+    for i in (seq $nums)
+        printf "\\$(printf '%o' $i)\n"
     end
 end
